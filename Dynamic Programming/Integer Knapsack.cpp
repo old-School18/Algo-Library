136fd63e6389be70c890b79a21427c3cf8c8627e
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2026-06-18 17:26:51 +0530

    Create Integer Knapsack.cpp

---
+++ Dynamic Programming/Integer Knapsack.cpp
@@ -0,0 +1,79 @@
+// Time Complexity: O(2^(n+W)) Recursive | O(n*W) for remaining
+// Memory Complexity: O(n+W) Recursive | O(n*W) Memoization | O(W) Tabulation
+// Fill up a knapsack with given weights such that the sum of values of chosen weights is maximum. Each weight can be added multiple times. The sack can be filled up to W capacity
+
+// Recursive
+ll unboundedKnapsackRecurse(vector<ll> &weights, vector<ll> &values, ll W, ll idx)
+{
+    if (W < 0)
+    {
+        return -1;
+    }
+    if (idx == weights.size())
+    {
+        return 0;
+    }
+
+    ll exclude = unboundedKnapsackRecurse(weights, values, W, idx + 1);
+    ll include = unboundedKnapsackRecurse(weights, values, W - weights[idx], idx);
+    if (include != -1)
+    {
+        include += values[idx];
+    }
+    return max(exclude, include);
+}
+
+ll unboundedKnapsack(vector<ll> &weights, vector<ll> &values, ll W)
+{
+    return unboundedKnapsackRecurse(weights, values, W, 0);
+}
+
+// Memoization (Top-down)
+ll unboundedKnapsackMemo(vector<ll> &weights, vector<ll> &values, vector<vector<ll>> &dp, ll W, ll idx)
+{
+    if (W < 0)
+    {
+        return -1;
+    }
+    if (idx == weights.size())
+    {
+        return 0;
+    }
+    if (dp[idx][W] != LLONG_MIN)
+    {
+        return dp[idx][W];
+    }
+
+    ll exclude = unboundedKnapsackMemo(weights, values, dp, W, idx + 1);
+    ll include = unboundedKnapsackMemo(weights, values, dp, W - weights[idx], idx);
+    if (include != -1)
+    {
+        include += values[idx];
+    }
+    return dp[idx][W] = max(exclude, include);
+}
+
+ll unboundedKnapsack(vector<ll> &weights, vector<ll> &values, ll W)
+{
+    ll n = weights.size();
+    vector<vector<ll>> dp(n + 1, vector<ll>(W + 1, LLONG_MIN));
+    return unboundedKnapsackMemo(weights, values, dp, W, 0);
+}
+
+// Tabulation (Bottom-up)
+ll unboundedKnapsack(vector<ll> &weights, vector<ll> &values, ll W)
+{
+    ll n = weights.size();
+    vector<ll> dp(W + 1, 0);
+    for (ll i = 0; i <= W; i++)
+    {
+        for (ll j = 0; j < n; j++)
+        {
+            if (i >= weights[j])
+            {
+                dp[i] = max(dp[i], dp[i - weights[j]] + values[j]);
+            }
+        }
+    }
+    return dp[W];
+}
